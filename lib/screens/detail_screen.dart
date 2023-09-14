c6736e9db3637aa23bb8d41e4e1d1098226f0aa4
Faisal Aslam <Faisalaslam218@gmail.com>
2023-09-14 13:43:24 -0700

    Place Order

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:food_app/provider/my_provider.dart';
 import 'package:food_app/order.dart/order.dart';
